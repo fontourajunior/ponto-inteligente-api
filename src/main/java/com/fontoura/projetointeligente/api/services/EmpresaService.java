fd6388a7d58e0aa659405a7b5fa05f3b7eaf59c3
Antonio F. Fontoura Junior <antonio.fontoura@zup.com.br>
2017-11-16 21:08:45 -0200

    add serviços

--- src/main/java/com/fontoura/projetointeligente/api/services/EmpresaService.java
+++ src/main/java/com/fontoura/projetointeligente/api/services/EmpresaService.java
@@ -1,4 +1,23 @@
-package com.fontoura.projetointeligente.api.services.impl;
+package com.fontoura.projetointeligente.api.services;
+
+import com.fontoura.projetointeligente.api.entities.Empresa;
+
+import java.util.Optional;
 
 public interface EmpresaService {
+
+    /**
+     * Retorna uma empresa dado um CNPJ
+     * @param cnpj
+     * @return Optional<Empresa>
+     */
+    Optional<Empresa> buscarPorCnpj(String cnpj);
+
+    /**
+     * Cadastra uma nvoa empresa na base da dados
+     * @param empresa
+     * @return Empresa
+     */
+    Empresa persistir(Empresa empresa);
+
 }
